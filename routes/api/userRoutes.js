5f743b597abf5eaf41908c3acbd32325fd739f85
cmarvann <covingtonmarvann@gmail.com>
2022-07-29 22:26:00 -0400

    create login route for authentication

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -36,20 +36,51 @@ router.get('/:id', (req, res) => {
   }); 
 });
 
-// POST /api/users
-router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+// // POST /api/users
+// router.post('/', (req, res) => {
+//   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+//   User.create({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password
+//   })
+//     .then(dbUserData => res.json(dbUserData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
 
-    });
+//     });
+// });
+
+router.post('/login', (req, res) => {
+  // Query operation
+
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+    // add comment syntax in front of this line in the .then()
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    // res.json({ user: dbUserData });
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    // conditional statement
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    
+
+  });  
 });
 
 // PUT /api/users/1
