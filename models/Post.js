0cf2589efd15fa40b3c78e94fdd2019bd90cc829
cmarvann <covingtonmarvann@gmail.com>
2022-07-30 21:17:31 -0400

    Enamle users to vote on post

--- models/Post.js
+++ models/Post.js
@@ -3,9 +3,33 @@ const sequelize = require('../config/connection');
 
 //Define the Post model 
 // initialize the Post model
+
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
 // columns, config naming, pass connection instance to initialize the Post model
+static upvote(body, models) {
+  return models.Vote.create({
+    user_id: body.user_id,
+    post_id: body.post_id
+  }).then(() => {
+    return Post.findOne({
+      where: {
+        id: body.post_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [
+          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+          'vote_count'
+        ]
+      ]
+    });
+  });
+}
+}
 
 Post.init(
   {
