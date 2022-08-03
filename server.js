736306d40d6e799855fbadaf9090325c3e6cc14c
cmarvann <covingtonmarvann@gmail.com>
2022-08-03 18:21:07 -0400

    Create Home Page

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+// const routes = require('./routes');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
+
+// // turn on routes
+// app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
