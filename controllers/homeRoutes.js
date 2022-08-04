6475b90c602f680adf820f0c536bddd2ecebcd07
cmarvann <covingtonmarvann@gmail.com>
2022-08-03 22:29:09 -0400

    create login and sign up page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,6 +5,8 @@ const { Post, User, Comment, Vote } = require('../models');
 // get all posts for homepage
 
 router.get('/', (req, res) => {
+  console.log(req.session);
+
   Post.findAll({
     attributes: [
       'id',
@@ -40,6 +42,14 @@ router.get('/', (req, res) => {
     });
 });
 
+// Check session and redirect to homepage
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
+  res.render('login');
+});
 
 module.exports = router;
